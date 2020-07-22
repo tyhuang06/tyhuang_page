b77f5c39da580d3776af3e4ceb9e38c1fedf0c60
tyhuang <tyhuang06@gmail.com>
2020-07-22 16:31:28 +0800

    Updates

---
+++ precache-manifest.3ad579d0db07f888ca3c4e52520f8915.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc8b6812060c461f687c7986e44cb0d",
+    "url": "/tyhuang_page/index.html"
+  },
+  {
+    "revision": "737b86a31f4c30a116b8",
+    "url": "/tyhuang_page/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3a30c9657678b45417c7",
+    "url": "/tyhuang_page/static/css/main.4a43c215.chunk.css"
+  },
+  {
+    "revision": "737b86a31f4c30a116b8",
+    "url": "/tyhuang_page/static/js/2.ae171e5f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tyhuang_page/static/js/2.ae171e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a30c9657678b45417c7",
+    "url": "/tyhuang_page/static/js/main.7b52053e.chunk.js"
+  },
+  {
+    "revision": "789a7aeceb5868be17a6",
+    "url": "/tyhuang_page/static/js/runtime-main.be4d640b.js"
+  },
+  {
+    "revision": "37abc5749a4d753c19d69d2f7e97a1fb",
+    "url": "/tyhuang_page/static/media/about_photo.37abc574.jpg"
+  },
+  {
+    "revision": "29f1283d151f83ffc083c37ca59f3e6e",
+    "url": "/tyhuang_page/static/media/bomberman.29f1283d.jpg"
+  },
+  {
+    "revision": "1e9253e233dddd1de107c993b87b10b7",
+    "url": "/tyhuang_page/static/media/landing.1e9253e2.jpg"
+  },
+  {
+    "revision": "6d3e94bebcbb90abd6017edbe8717902",
+    "url": "/tyhuang_page/static/media/laptop.6d3e94be.jpg"
+  },
+  {
+    "revision": "ba0c31a9e1fbb31cb7a1e408ab01802a",
+    "url": "/tyhuang_page/static/media/portfolio.ba0c31a9.jpg"
+  },
+  {
+    "revision": "1f7a3e5fe158af9f7251e1a1144b321a",
+    "url": "/tyhuang_page/static/media/resume_tw.1f7a3e5f.pdf"
+  },
+  {
+    "revision": "c602c1b4b35f4ec48135a9eba27f7977",
+    "url": "/tyhuang_page/static/media/yelpcamp.c602c1b4.jpg"
+  }
+]);
