3525cac1ef7babd147f9b9265b03dd39c463e59e
tyhuang <tyhuang06@gmail.com>
2020-07-23 00:55:09 +0800

    update info

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,7 +10,7 @@ class Intro extends React.Component {
                 <Jumbotron id='intro_content'>
                     <h1>Hi! I'm Tzu-Yu Huang</h1>
                     <p>
-                    2021年秋季打算赴美攻讀資工碩士，正在尋找一年左右的實習、正職機會。
+                    
                     </p>
                 </Jumbotron>
             </Container>
